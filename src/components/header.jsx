e538c78dbc5143e825e5a2c3ff847e025e1eedff
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-09-19 07:48:03 +0100

    create new pages and components

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <>
+      <header>
+        <div></div>
+        <div>
+            <Link></Link>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
