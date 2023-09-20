981730eb1e35f31dd62d2fdfdac3b7133783878d
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-09-20 15:35:49 +0100

    start homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Button from "./components/button";
+import Underline from "./assets/curved_underline.png";
+import hero_img from "./assets/hero_img.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      {/* hero */}
+      <div
+        className=" bg-background-col text-white flex flex-col
+       lg:flex-row justify-center items-center"
+      >
+        <div>
+          <p>Igniting a Revolution in HR Innovation</p>
+          <img src={Underline} alt="underline-img" />
+        </div>
+        <div>
+          <div>
+            <h2>getlinked Tech</h2>
+            <h2>
+              Hackathon <span>1.0</span>
+            </h2>
+          </div>
+          <p>
+            Participate in getlinked tech Hackathon 2023 stand a chance to win a
+            Big prize
+          </p>
+          <Button />
+        </div>
+        <div>
+          <img src={hero_img} alt="" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
