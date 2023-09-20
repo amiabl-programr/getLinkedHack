f19e4f7f94f625ae1f074ab7198099a659849a47
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-09-20 15:36:37 +0100

    add footer and header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,28 @@
+import Logo from "../assets/getlinkedlogo.png";
+import menu from "../assets/menu.png";
+import Button from "./button";
 import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header>
-        <div></div>
+      <header
+        className="flex items-center justify-around bg-background-col
+       text-white pt-[34px] lg:pt-[63px] pb-4 lg:pb-[25px]"
+      >
         <div>
-            <Link></Link>
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className=" hidden lg:flex gap-14 items-center rounded">
+          <Link>Timeline</Link>
+          <Link>Overview</Link>
+          <Link>FAQs</Link>
+          <Link>Contact</Link>
+          <Button />
+        </div>
+
+        <div className="lg:hidden">
+          {" "}
+          <img src={menu} alt="menu" />
         </div>
       </header>
     </>
